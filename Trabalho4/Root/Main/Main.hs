d109d5ef5cd3f6fddcaf103b2bc056519ecb2460
Caetano Korilo <caefleury15@gmail.com>
2023-12-17 12:05:26 -0300

    trab 4 - Q1

---
+++ Trabalho4/Root/Main/Main.hs
@@ -0,0 +1,12 @@
+module Root.Main.Main where
+
+main = do
+    putStrLn "## TRABALHO 04 -- Linguagens de Programacao 2023.2 UnB ##"
+
+    putStrLn "## Q1 ##"
+    
+
+
+    putStrLn "## Q2 ##"
+    putStrLn "## Q3 ##"
+    putStrLn "## Q4 ##"
